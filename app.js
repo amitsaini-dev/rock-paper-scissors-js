2913d68e96bbc144fc0173ca3802a9d6ebcf2b6d
Amit Saini <amitsaini.ce@gmail.com>
2025-07-29 23:57:23 +0530

    feat: Implemented full Rock-Paper-Scissors game logic with score tracking and reset feature

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 let userChoices = document.querySelectorAll(".buttons");
 let userInput = document.querySelector("#user-input");
 let computerInput = document.querySelector("#computer-input");
-
 let comChoices = ["Rock", "Paper", "Scissors"];
+let gameStatus = document.querySelector(".game-status");
+let comScoreDisp = document.querySelector("#comScoreDisp");
+let userScoreDisp = document.querySelector("#userScoreDisp");
+let userScore = 0;
+let computerScore = 0;
 
 userChoices.forEach(button => {
     button.addEventListener("click", function () {
+
         userChoice = this.id;
         userInput.textContent = userChoice;
         let comInput = computersRandomInput();
         computerInput.textContent = comInput;
+
+        console.log(comInput);
+
+        if (userChoice === comInput) {
+            gameStatus.textContent = "Draw";
+        }
+        else if (
+            (userChoice == "Rock" && comInput == "Scissors") ||
+            (userChoice == "Paper" && comInput == "Rock") ||
+            (userChoice == "Scissors" && comInput == "Paper")) {
+            gameStatus.textContent = "You Win";
+            userScore++;
+        }
+        
+        else {
+            gameStatus.textContent = "Computer Wins";
+            computerScore++;
+        }
+
+        userScoreDisp.textContent = userScore;
+        comScoreDisp.textContent = computerScore;
     })
 })
 
@@ -18,3 +44,18 @@ function computersRandomInput() {
     let comInput = comChoices[random];
     return comInput;
 }
+
+document.querySelector(".reset").addEventListener("click", () => {
+
+    userScore = 0;
+    computerScore = 0;
+
+    userScoreDisp.textContent = 0;
+    comScoreDisp.textContent = 0;
+
+    userInput.textContent = "-";
+    computerInput.textContent = "-";
+
+    gameStatus.textContent = "Ready to play? Pick Rock, Paper, or Scissors!";
+}
+);
